612b67f07a26d8239a447154b1b6620d13124f15
tsuchiya kohei <tsuchiya@MacBook-Air-2.local>
2020-03-16 19:57:58 +0900

    ２点間の距離を求める原型を作成

--- main.go
+++ main.go
@@ -7,6 +7,19 @@ import (
 	melody "gopkg.in/olahol/melody.v1"
 )
 
+// 2点間の距離を計算して半径50m以内だったらtrue
+func isClose(lat1, lng1, lat2, lng2 interface{}) bool {
+	const setznaDistance float64 = 50
+	// TODO: 三平方の定理などで２点間の距離を出す
+	// 緯度1度あたり110.9463km、経度1度あたり90.4219km
+	// https://s-giken.info/distance/distance.php
+
+	// HACK: 地球の丸みを考慮すると、三平方の定理では不十分なので改善する。
+	// distance := math.Sqrt(math.Pow((lat1-lat2)*110.9463, 2) + math.Pow((lng1-lng2)*90.4219, 2))
+	// return distance < setznaDistance
+	return true
+}
+
 func main() {
 	r := gin.Default()
 	m := melody.New()
@@ -26,13 +39,23 @@ func main() {
 	})
 
 	r.GET("/room/ws", func(c *gin.Context) {
+		// TODO: コネクション接続時に初期位置情報をセットする
 		m.HandleRequest(c.Writer, c.Request)
 	})
 
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		// TODO: メッセージの度に位置情報を更新する
+
 		m.BroadcastFilter(msg, func(q *melody.Session) bool {
-			// ここで半径50mのユーザーに絞る
-			return true
+			lat1, isExistLat1 := s.Get("latitude")
+			lng1, isExistLng1 := s.Get("longitude")
+			lat2, isExistLat2 := q.Get("latitude")
+			lng2, isExistLng2 := q.Get("longitude")
+			if !isExistLat1 || !isExistLng1 || !isExistLat2 || !isExistLng2 {
+				return false
+			}
+
+			return isClose(lat1, lng1, lat2, lng2)
 		})
 	})
 	r.Run(":8080")
