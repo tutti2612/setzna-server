a7cfacc6472f3338e35f1e5b7b125ff939f22056
k.tsuchiya <doyahirohira@gmail.com>
2020-03-28 01:52:51 +0900

    DB情報を環境変数から取得するように変更

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,14 +1,23 @@
 package db
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func Connection() *gorm.DB {
-	db, err := gorm.Open("mysql", "setzna:setzna@(mysql)/setzna?charset=utf8&parseTime=True&loc=Local")
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, pass, host, dbName))
 	if err != nil {
-		panic("failed to connect database")
+		log.Fatal("failed to connect database")
 	}
 	db.LogMode(true)
 	return db
