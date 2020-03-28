3397daf0c200c31fbbf74a200fdf052b7bf9dec4
k.tsuchiya <doyahirohira@gmail.com>
2020-03-28 22:19:15 +0900

    assertライブラリを使うのを止めた

--- pkg/location/location_test.go
+++ pkg/location/location_test.go
@@ -2,10 +2,31 @@ package location
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestDistance(t *testing.T) {
-	assert.Equal(t, 3.523778708998686, Distance(35.658482, 139.701441, 35.690224, 139.700089))
+	type args struct {
+		lat1 float64
+		lng1 float64
+		lat2 float64
+		lng2 float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			"渋谷と新宿の距離",
+			args{35.658482, 139.701441, 35.690224, 139.700089},
+			3.523778708998686,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Distance(tt.args.lat1, tt.args.lng1, tt.args.lat2, tt.args.lng2); got != tt.want {
+				t.Errorf("Distance() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
